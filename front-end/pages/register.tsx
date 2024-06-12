a84d0562d0c169c6f36049645045d790053e1a32
Colombao <joaoocolombo@gmail.com>
2024-06-12 20:48:30 -0300

    chore: Remove unused code and dependencies

--- front-end/pages/register.tsx
+++ front-end/pages/register.tsx
@@ -5,11 +5,6 @@ import React from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome6";
 import * as Yup from "yup";
-import { Formik } from "formik";
-import React from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome6";
-import * as Yup from "yup";
 
 type RootStackParamList = {
   Landing: undefined;
@@ -35,21 +30,9 @@ const RegisterPage = () => {
       .oneOf([Yup.ref("password"), ""], "As senhas devem corresponder")
       .required("Confirmação de senha é obrigatória"),
   });
-  const validationSchema = Yup.object().shape({
-    fullName: Yup.string().required("Nome completo é obrigatório"),
-    cpf: Yup.string()
-      .required("CPF é obrigatório")
-      .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "CPF inválido"),
-    email: Yup.string().email("Email inválido").required("Email é obrigatório"),
-    password: Yup.string()
-      .required("Senha é obrigatória")
-      .min(6, "A senha deve ter pelo menos 6 caracteres"),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref("password"), ""], "As senhas devem corresponder")
-      .required("Confirmação de senha é obrigatória"),
-  });
 
   const handleRegister = (values: any) => {
+    // Função de registro
     console.log("teste", values);
   };
 
@@ -60,7 +43,7 @@ const RegisterPage = () => {
   const formatCPF = (value: string) => {
     return value
       .replace(/\D/g, "")
-      .replace(/(\d{3})(\d)/, "$1.$2") 
+      .replace(/(\d{3})(\d)/, "$1.$2")
       .replace(/(\d{3})(\d)/, "$1.$2")
       .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
   };
@@ -85,6 +68,7 @@ const RegisterPage = () => {
             handleBlur,
             handleSubmit,
             values,
+            setFieldValue,
             errors,
             touched,
           }) => (
@@ -146,33 +130,7 @@ const RegisterPage = () => {
                   <Text className="text-red-600">{errors.password}</Text>
                 )}
               </View>
-              <View className="w-full mt-4 px-10">
-                <Text className="text-lg text-red-900">Senha:</Text>
-                <TextInput
-                  className="h-[40px] mt-[5px] px-[10px] bg-[#D9D9D9] border-[1px] border-[#676767]"
-                  secureTextEntry={true}
-                  value={values.password}
-                  onChangeText={handleChange("password")}
-                  onBlur={handleBlur("password")}
-                />
-                {touched.password && errors.password && (
-                  <Text className="text-red-600">{errors.password}</Text>
-                )}
-              </View>
 
-              <View className="w-full mt-4 px-10">
-                <Text className="text-lg text-red-900">Repita a Senha:</Text>
-                <TextInput
-                  className="h-[40px] mt-[5px] px-[10px] bg-[#D9D9D9] border-[1px] border-[#676767]"
-                  secureTextEntry={true}
-                  value={values.confirmPassword}
-                  onChangeText={handleChange("confirmPassword")}
-                  onBlur={handleBlur("confirmPassword")}
-                />
-                {touched.confirmPassword && errors.confirmPassword && (
-                  <Text className="text-red-600">{errors.confirmPassword}</Text>
-                )}
-              </View>
               <View className="w-full mt-4 px-10">
                 <Text className="text-lg text-red-900">Repita a Senha:</Text>
                 <TextInput
