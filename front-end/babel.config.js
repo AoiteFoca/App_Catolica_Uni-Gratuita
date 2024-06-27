37a01a492c7000564af4590f37dc91a51ff9f7b0
Nicolas Gustavo Conte <80847876+MrNicolass@users.noreply.github.com>
2024-06-26 21:38:01 -0300

    Feat: Last Changes after N3

--- front-end/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"], // ou outros presets que você usa
-    plugins: ["tailwindcss-react-native/babel"],
-  };
-};
