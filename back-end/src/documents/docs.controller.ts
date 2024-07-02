f4fc5d5b1ccbbaa1b1676b7c379a06e244a52913
Nicolas Gustavo Conte <80847876+MrNicolass@users.noreply.github.com>
2024-07-01 22:25:19 -0300

    Now we can save docs in a determined folder

--- back-end/src/documents/docs.controller.ts
+++ back-end/src/documents/docs.controller.ts
@@ -1,29 +1,56 @@
 import {
-    Body,
-    Controller,
-    Get,
-    HttpStatus,
-    Param,
-    Patch,
-    Post,
-    Request,
-    Res,
-    UploadedFile,
-    UseInterceptors,
-  } from '@nestjs/common';
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Logger,
+  Patch,
+  Post,
+  Request,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Express, Response } from 'express';
 import { DocsService } from './docs.service';
-import { UsersService } from 'src/users/users.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
-  
-  @Controller('docs')
-  export class DocsController {
-    constructor(private prisma: UsersService) {}
+@Controller('docs')
+export class DocsController {
+  constructor(private docs: DocsService) {}
+  private readonly log = new Logger(DocsService.name);
 
-    @Post('upload')
-    @UseInterceptors(FileInterceptor('file'))
-    uploadFile(@UploadedFile() file: Express.Multer.File) {
-        console.log(file);
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: 'src/documents/files/',
+        filename: (req, file, save) => {
+          const filename = `${Date.now()}-${file.originalname.replace(/\s/g, '')}`;
+          save(null, filename);
+        },
+      }),
+    }),
+  )
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() data: any,
+    @Res() res: Response
+  ): Promise<any> {
+    try {
+      const result = await this.docs.saveFile(file,data);
+      return res.status(HttpStatus.CREATED).json({
+        success: true,
+        message: 'Arquivo salvo com sucesso!',
+        data: result,
+      });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        success: false,
+        message: 'Erro ao salvar arquivo!',
+        error: error.message,
+      });
     }
   }
+}
--- back-end/src/documents/docs.service.ts
+++ back-end/src/documents/docs.service.ts
@@ -1,8 +1,24 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Usuario } from '@prisma/client';
-import { compare, hash } from 'bcrypt';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class DocsService {
+    constructor(private readonly prisma: PrismaService) {}
+    private readonly log = new Logger(UsersService.name);
+
+    async saveFile(file: Express.Multer.File, data: any): Promise<any> {
+        try{
+            const docs = this.prisma.documentos.create({
+                data: {
+                    pessoaId: 1,
+                    inscricao: file.filename,
+                }
+            })
+        }catch(error){
+            this.log.log(`Arquivo "${file.filename}" não salvo!`);
+            throw new Error('Não foi possível gravar o arquivo!');
+        }
+    
+    }
 }
